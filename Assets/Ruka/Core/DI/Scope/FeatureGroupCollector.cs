030bbbb5b9dbb47555a3c98f73a6017d5cf3a8ce
azaneNH37 <Anventie37000631@outlook.com>
2026-05-17 16:04:55 +0800

    pkg: 移除TriInspector依赖

--- Assets/Ruka/Core/DI/Scope/FeatureGroupCollector.cs
+++ Assets/Ruka/Core/DI/Scope/FeatureGroupCollector.cs
@@ -8,7 +8,7 @@ namespace Ruka.Core.DI
     public sealed class FeatureGroupCollector : ScriptableObject
     {
         [SerializeField, SymbolSelector] private Symbol<InstallerGroup> targetGroup;
-        [SerializeField] private List<string> qualifiedTypes = new();
+        [SerializeField, HideInInspector] private List<string> qualifiedTypes = new();
 
         public Symbol<InstallerGroup> TargetGroup => targetGroup;
         public IReadOnlyList<string> QualifiedTypes => qualifiedTypes;
